cd39a489a5f15c0ca318f629c3cb9a0e7df6b67f
INGLIING <frok2005@mail.ru>
2015-11-10 12:01:54 +0300

    Создан AddEmployeeTransaction и тесты

---
+++ TestEmployeeToString/PayrollTestToString_Rylkov.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payroll_Rylkov;
+
+namespace TestEmployeeToString
+{
+    [TestClass]
+    public class PayrollTestToString_Rylkov
+    {
+        [TestMethod]
+        public void TestEmployeeToString()
+        {
+            int empId = 1;
+            Employee e = new Employee(empId, "Bob", "Home");
+            Assert.AreEqual("Bob", e.Name);
+            Assert.AreEqual("Home", e.Address);
+            Assert.AreEqual(empId, e.EmpId);
+        }
+        
+    }
+}
